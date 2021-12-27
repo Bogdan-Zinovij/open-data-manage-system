352d4ca1a1ab174208ea32b17c07d9562e7cb088
artemmatiushenko1 <artom.matyushenko@gmail.com>
2021-12-27 12:30:43 +0200

    Created controller functions for creating, deleting and updating grants

--- src/js/controllers/grantController.js
+++ src/js/controllers/grantController.js
@@ -38,3 +38,70 @@ exports.getGrant = async (req, reply) => {
     });
   }
 };
+
+exports.createGrant = async (req, reply) => {
+  try {
+    const newGrant = await Grant.create(req.body);
+    reply.status(201).send({
+      status: 'success',
+      data: { newGrant },
+    });
+  } catch (err) {
+    reply.status(404).send({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+exports.deleteGrant = async (req, reply) => {
+  try {
+    const { id } = req.params;
+    const grant = await Grant.findOne({ where: { id } });
+
+    if (!grant)
+      throw new Error(`Can't delete a grant with an id value of ${id}`);
+
+    await Grant.destroy({ where: { id } });
+
+    reply.status(200).send({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    reply.status(404).send({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+exports.updateGrant = async (req, reply) => {
+  try {
+    const { id } = req.params;
+    const grant = await Grant.findOne({ where: { id } });
+
+    if (!grant) throw new Error(`There's no grant with an id value of ${id}`);
+
+    const [affectedRowsCount] = await Grant.update(req.body, {
+      where: { id },
+    });
+
+    if (!affectedRowsCount)
+      throw new Error(`Grant with an id value of ${id} hasn't been updated`);
+
+    const updatedRole = await Grant.findOne({ where: { id } });
+
+    reply.status(200).send({
+      status: 'success',
+      data: {
+        updatedRole,
+      },
+    });
+  } catch (err) {
+    reply.status(404).send({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
