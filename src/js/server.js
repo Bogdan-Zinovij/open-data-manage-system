3104a31843fd46b1393a9008912eeacf53a5387a
artemmatiushenko1 <artom.matyushenko@gmail.com>
2021-12-31 15:25:20 +0200

    Added tests for actionType table related endpoints

--- src/js/server.js
+++ src/js/server.js
@@ -10,7 +10,6 @@ const PORT = process.env.FASTIFY_PORT;
 (async () => {
   associate();
   await db.sync({ force: false });
-
   app.listen(PORT || 3000, process.env.FASTIFY_HOST || '127.0.0.1', err => {
     if (err) {
       app.log.error(err);
